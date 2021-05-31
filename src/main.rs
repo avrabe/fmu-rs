35d8412c4e432d92e26778ca0180718b8a50bdb3
Ralf Anton Beier <ralf_beier@me.com>
2021-05-31 06:10:55 +0200

    CMD line compatibility.
    Use the same command line parameters as fullmetalupdate.
    Add in logging support.
    
    Signed-off-by: Ralf Anton Beier <ralf_beier@me.com>

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,11 @@ use hawkbit::ddi::{Client, Execution, Finished};
 use ini::{Ini, Properties};
 use ostree::RepoMode;
 use serde::Serialize;
+use std::fs;
 use tokio::time::sleep;
 
-use std::fs;
+use tracing::{info, Level};
+use tracing_subscriber::FmtSubscriber;
 
 pub fn path_exists(path: &str) -> bool {
     fs::metadata(path).is_ok()
@@ -30,7 +32,6 @@ mod tests {
 pub fn get_ini_string(section: &Properties, name: &str) -> String {
     section.get(name).unwrap().to_string()
 }
-
 
 pub fn get_ini_bool(section: &Properties, name: &str) -> bool {
     section.get(name).unwrap().parse().unwrap()
@@ -85,9 +86,9 @@ struct Opts {
     /// Sets a custom config file. Could have been an Option<T> with no default too
     #[clap(short, long, default_value = "config.cfg")]
     config: String,
-    /// A level of verbosity, and can be used multiple times
-    #[clap(short, long, parse(from_occurrences))]
-    verbose: i32,
+    /// Print debug information
+    #[clap(short)]
+    debug: bool,
 }
 
 #[derive(Debug, Serialize)]
@@ -99,71 +100,103 @@ pub(crate) struct ConfigData {
 #[derive(Debug)]
 struct HawkbitOpts {
     hostname: String,
-    ssl: bool,
     tenant_id: String,
     target_name: String,
     auth_token: String,
     hawkbit_vendor_name: String,
-    log_level: String,
 }
 
 #[derive(Debug)]
 struct OstreeOpts {
+    hostname: String,
     ostree_name_remote: String,
     ostree_gpg_verify: bool,
-    ostree_ssl: bool,
-    ostree_url_port: String,
     ostreepush_ssh_port: String,
     ostreepush_ssh_user: String,
     ostreepush_ssh_pwd: String,
 }
 
+pub fn get_log_level(level: &String) -> Level {
+    match level.to_uppercase().as_ref() {
+        "DEBUG" => return Level::DEBUG,
+        "INFO" => return Level::INFO,
+        "WARN" => return Level::WARN,
+        "ERROR" => return Level::ERROR,
+        "FATAL" => return Level::ERROR,
+        _ => return Level::INFO,
+    }
+}
+
 #[tokio::main]
-async fn main() -> Result<(), ()> {
+async fn main() {
+    // Get the command line arguments
     let opts: Opts = Opts::parse();
 
-    // Gets a value for config if supplied by user, or defaults to "default.conf"
-    println!("Value for config: {}", opts.config);
+    // Read from the configuration file
+    let conf = Ini::load_from_file(&opts.config).unwrap();
 
-    // Vary the output based on how many times the user used the "verbose" flag
-    // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
-    match opts.verbose {
-        0 => println!("No verbose info"),
-        1 => println!("Some verbose info"),
-        2 => println!("Tons of verbose info"),
-        3 | _ => println!("Don't be crazy"),
-    }
-
-    let conf = Ini::load_from_file(opts.config).unwrap();
-
+    // Read the server information
     let section = conf.section(Some("server")).unwrap();
     let server_host_name = get_ini_string(section, "server_host_name");
 
     let section = conf.section(Some("client")).unwrap();
-    let hawkbit_url_port = get_ini_string(section, "hawkbit_url_port");
+    let log_level = if opts.debug {
+        "debug".to_string()
+    } else {
+        get_ini_string(section, "log_level")
+    };
 
+
+    // a builder for `FmtSubscriber`.
+    let subscriber = FmtSubscriber::builder()
+        // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
+        // will be written to stdout.
+        .with_max_level(get_log_level(&log_level))
+        // completes the builder.
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("setting default subscriber failed");
+    info!("Value for config: {}", opts.config);
+    info!("LogLevel: {}", log_level);
+
+    let hawkbit_url_port = get_ini_string(section, "hawkbit_url_port");
+    let hawkbit_ssl= get_ini_bool(section, "hawkbit_ssl");
+
+    let hawkbit_url_type = if hawkbit_ssl {
+        "https://"
+    } else {
+        "http://"
+    };
+    // setup hawkbit
     let hawkbit_opts: HawkbitOpts = HawkbitOpts {
         hawkbit_vendor_name: get_ini_string(section, "hawkbit_vendor_name"),
-        ssl: get_ini_bool(section, "hawkbit_ssl"),
         tenant_id: get_ini_string(section, "hawkbit_tenant_id"),
         target_name: get_ini_string(section, "hawkbit_target_name"),
         auth_token: get_ini_string(section, "hawkbit_auth_token"),
-        log_level: get_ini_string(section, "log_level"),
-        hostname: format!("http://{}:{}", server_host_name, hawkbit_url_port),
+        hostname: format!("{}{}:{}", hawkbit_url_type, server_host_name, hawkbit_url_port),
     };
-    println!("{:?}", hawkbit_opts);
+    info!("{:?}", hawkbit_opts);
 
+    // setup ostree
     let section = conf.section(Some("ostree")).unwrap();
+    let ostree_ssl= get_ini_bool(section, "ostree_ssl");
+    let ostree_url_port= get_ini_string(section, "ostree_url_port");
+    let ostree_url_type = if ostree_ssl {
+        "https://"
+    } else {
+        "http://"
+    };
+
     let ostree_opts: OstreeOpts = OstreeOpts {
+        hostname: format!("{}{}:{}", ostree_url_type,server_host_name, ostree_url_port),
         ostree_name_remote: get_ini_string(section, "ostree_name_remote"),
         ostree_gpg_verify: get_ini_bool(section, "ostree_gpg-verify"),
-        ostree_ssl: get_ini_bool(section, "ostree_ssl"),
-        ostree_url_port: get_ini_string(section, "ostree_url_port"),
         ostreepush_ssh_user: get_ini_string(section, "ostreepush_ssh_user"),
         ostreepush_ssh_pwd: get_ini_string(section, "ostreepush_ssh_pwd"),
         ostreepush_ssh_port: get_ini_string(section, "ostreepush_ssh_port"),
     };
-    println!("{:?}", ostree_opts);
+    info!("{:?}", ostree_opts);
     // more program logic goes here...
 
     //let _repo = &ostree::Repo::checkout_at(&repo, o, libc::AT_FDCWD, "./download/", "init", gio::NONE_CANCELLABLE);
@@ -175,12 +208,13 @@ async fn main() -> Result<(), ()> {
         &hawkbit_opts.auth_token,
     )
     .unwrap();
-    loop {
+
+     loop {
         let reply = ddi.poll().await.expect("buh");
         dbg!(&reply);
 
         if let Some(request) = reply.config_data_request() {
-            println!("Uploading config data");
+            info!("Uploading config data");
             let data = ConfigData {
                 hw_revision: "1.0".to_string(),
             };
@@ -192,7 +226,7 @@ async fn main() -> Result<(), ()> {
         }
 
         if let Some(update) = reply.update() {
-            println!("Pending update");
+            info!("Pending update");
 
             let update = update.fetch().await;
             let update = match update {
@@ -207,21 +241,9 @@ async fn main() -> Result<(), ()> {
                 .expect("ff");
 
             for chunk in update.chunks() {
-                print!("Retrieving {}\n", chunk.name());
+                //info!("Retrieving {}\n", chunk.name());
                 get_repo(chunk.name());
             }
-            //let artifacts = update.download(Path::new("./download/")).await.expect("kkkk");
-            //dbg!(&artifacts);
-
-            //#[cfg(feature = "hash-digest")]
-            //for artifact in artifacts {
-            //    #[cfg(feature = "hash-md5")]
-            //    artifact.check_md5().await?;
-            //    #[cfg(feature = "hash-sha1")]
-            //    artifact.check_sha1().await?;
-            //    #[cfg(feature = "hash-sha256")]
-            //    artifact.check_sha256().await?;
-            //}
 
             update
                 .send_feedback(Execution::Closed, Finished::Success, vec![])
@@ -230,7 +252,7 @@ async fn main() -> Result<(), ()> {
         }
 
         let t = reply.polling_sleep().expect("fff");
-        println!("sleep for {:?}", t);
+        info!("sleep for {:?}", t);
         sleep(t).await;
-    }
+    }; 
 }
